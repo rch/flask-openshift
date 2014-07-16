17057e0500ecfde34cb501e4ec3f3d08204e3828
Ryan Hill <rch@computer.org>
2014-07-16 10:18:15 -0600

    flaskr

--- wsgi/container/main.py
+++ wsgi/container/main.py
@@ -1,44 +1,78 @@
-from flask import Response, render_template, current_app, make_response
+import os, flask, utils
+from flask import Response, url_for, render_template, current_app, make_response
 from datetime import datetime, timedelta
 from container import app
-from container.tasks import add_together
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+app.config.update(dict(
+    ADMINDB=os.path.join(app.root_path, 'admin.db'),
+    DEBUG=True,
+    SECRET_KEY='public string',
+    ADMINUSER='admin',
+    ADMINPASS='default'
+))
+app.config.from_envvar('FLASK_MAIN', silent=True)
 
 
-@app.route("/admin")
-@app.route("/admin/<int:chk>")
-@app.route("/admin.html")
-def admin(chk=0):
-    return render_template('admin.html', msg=chk)
+@app.route('/')
+def show_entries():
+    db = utils.get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('base-view.html', entries=entries)
 
 
-@app.route("/robots.txt")
-def robots():
-    """Generate robots.txt. TODO: make allow/disallow rules."""
-    robots_txt = render_template('robots.txt')
-    response = make_response(robots_txt)
-    response.headers["Content-Type"] = 'text/plain'
-    
-    return response
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not flask.session.get('logged_in'):
+        abort(401)
+    db = utils.get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [flask.request.form['title'], flask.request.form['text']])
+    db.commit()
+    flask.flash('New entry was successfully posted')
+    return flask.redirect(url_for('show_entries'))
 
 
-@app.route('/sitemap.xml', methods=['GET'])
-def sitemap():
-      """Generate sitemap.xml. Makes a list of urls and date modified."""
-      pages=[]
-      ten_days_ago = (datetime.now() - timedelta(days=10)).date().isoformat()
-      # static pages
-      for rule in current_app.url_map.iter_rules():
-          if "GET" in rule.methods and len(rule.arguments)==0:
-              pages.append(
-                           [rule.rule,ten_days_ago]
-                           )
-      sitemap_xml = render_template('sitemap.xml', pages=pages)
-      response = make_response(sitemap_xml)
-      response.headers["Content-Type"] = 'text/xml'
-      
-      return response
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if flask.request.method == 'POST':
+        if flask.request.form['username'] != app.config['ADMINUSER']:
+            error = 'Invalid username'
+        elif flask.request.form['password'] != app.config['ADMINPASS']:
+            error = 'Invalid password'
+        else:
+            flask.session['logged_in'] = True
+            flask.flash('You were logged in')
+            return flask.redirect(url_for('show_entries'))
+    return render_template('base-login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    flask.session.pop('logged_in', None)
+    flask.flash('You were logged out')
+    return flask.redirect(url_for('show_entries'))
+
+
+@app.route('/initdb')
+def init_db():
+    """Creates the database tables."""
+    utils.init_db()
+    return 'Initialized the database.'
+
+
+# @app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    utils.init_db()
+    print('Initialized the database.')
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    current_db = utils.get_db()
+    if current_db is not None:
+        current_db.close()
--- wsgi/container/utils.py
+++ wsgi/container/utils.py
@@ -23,3 +23,28 @@ def register_blueprints(app, mapping=None):
     app.register_blueprint(pkgs.common.bp, url_prefix='/common')
     app.register_blueprint(pkgs.bootstrap.bp, url_prefix='/bootstrap')
     app.register_blueprint(pkgs.ngstrap.bp, url_prefix='/ngstrap')
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(flask.current_app.config['ADMINDB'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with flask.current_app.open_resource('admin.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(flask.g, 'sqlite_db'):
+        flask.g.sqlite_db = connect_db()
+    return flask.g.sqlite_db
+
