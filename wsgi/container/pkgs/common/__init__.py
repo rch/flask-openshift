fcede3fc6d90d2fa241d3fd83e8b985efc484016
Ryan Hill <rch@computer.org>
2014-07-16 09:04:54 -0600

    tasks

--- wsgi/container/pkgs/common/__init__.py
+++ wsgi/container/pkgs/common/__init__.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Blueprint
 from container.tasks import add_together
 
@@ -8,4 +9,4 @@ bp = Blueprint('common', __name__, static_folder='static', template_folder='temp
 @bp.route("/ping/<int:a>/<int:b>/")
 def ping(a=0, b=0):
     val = add_together(a, b)
-    return render_template('ping.html', msg=val)
+    return flask.render_template('ping.html', msg=val)
